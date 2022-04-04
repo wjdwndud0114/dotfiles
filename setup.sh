f72c647455154a82e50016a9d8a358154aa20c24
Kevin Jeong <wjdwndud0114@yahoo.com>
2022-04-04 16:21:08 -0700

    feat: add git-delta

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ ln -sf $DIR/.tmux.conf ~/.tmux.conf
 ln -sf $DIR/.zshenv ~/.zshenv
 ln -sf $DIR/.zalias ~/.zalias
 ln -sf $DIR/.zshrc ~/.zshrc
+ln -sf $DIR/.gitconfig ~/.gitconfig
 for file in $DIR/.config/*; do
   ln -sf $file ~/.config/
 done
@@ -26,6 +27,7 @@ brew install rg
 brew install vim
 brew install tmux
 brew install exa
+brew install git-delta
 
 echo "Setting up TERM with xterm-256color-italic"
 curl -L https://gist.githubusercontent.com/sos4nt/3187620/raw/bca247b4f86da6be4f60a69b9b380a11de804d1e/xterm-256color-italic.terminfo -o $DIR/xterm-256color-italic.terminfo
