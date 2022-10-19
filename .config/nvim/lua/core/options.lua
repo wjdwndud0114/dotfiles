01744c13ebf73521e4d13993745e0f23b2c8beca
Kevin Jeong <wjdwndud0114@yahoo.com>
2022-10-19 16:24:41 -0400

    fix: undo cmdheight change

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -78,7 +78,7 @@ local function load_options()
     helpheight     = 12;
     previewheight  = 12;
     showcmd        = false;
-    cmdheight      = 0;
+    cmdheight      = 1;
     cmdwinheight   = 5;
     equalalways    = false;
     laststatus     = 2;
