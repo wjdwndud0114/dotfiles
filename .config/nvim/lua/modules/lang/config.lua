be44f14cf13341a4982f59efa26f24acffc18481
Kevin Jeong <wjdwndud0114@yahoo.com>
2023-05-21 21:42:44 -0700

    feat(nvim): small treesitter change for vimspector

--- .config/nvim/lua/modules/lang/config.lua
+++ .config/nvim/lua/modules/lang/config.lua
@@ -3,10 +3,14 @@ local config = {}
 function config.nvim_treesitter()
   vim.api.nvim_command('set foldmethod=expr')
   vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
-  require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"bash", "comment", "css", "dockerfile", "go", "graphql", "help", "html", "http", "java", "javascript", "jsdoc", "json", "json5", "latex", "lua", "make", "perl", "python", "regex", "ruby", "rust", "scss", "tsx", "typescript", "vim", "yaml"},
+  require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "markdown", "markdown_inline", "bash", "comment", "css", "dockerfile", "go", "graphql", "html",
+      "http", "java", "javascript",
+      "jsdoc", "json", "json5", "latex", "lua", "make", "perl", "python", "regex", "ruby", "rust", "scss", "tsx",
+      "typescript", "vim", "yaml" },
     highlight = {
       enable = true,
+      disable = { "VimspectorPrompt" },
     },
     textobjects = {
       select = {
