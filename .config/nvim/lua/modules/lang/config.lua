44cd6168500ce4bb66699509a9f29e0f4a8e2087
Kevin Jeong <wjdwndud0114@yahoo.com>
2025-12-23 15:20:47 -0800

    feat: update treesitter to main

--- .config/nvim/lua/modules/lang/config.lua
+++ .config/nvim/lua/modules/lang/config.lua
@@ -1,31 +1,74 @@
 local config = {}
 
 function config.nvim_treesitter()
-  vim.api.nvim_command('set foldmethod=expr')
-  vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
-  require 'nvim-treesitter.configs'.setup {
-    ensure_installed = { "markdown", "markdown_inline", "bash", "comment", "css", "dockerfile", "go", "graphql", "html",
-      "http", "java", "javascript",
-      "jsdoc", "json", "json5", "latex", "lua", "make", "perl", "python", "regex", "ruby", "rust", "scss", "tsx",
-      "typescript", "vim", "yaml" },
-    highlight = {
-      enable = true,
-      disable = { "VimspectorPrompt" },
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-          ["ab"] = "@block.outer",
-          ["ib"] = "@block.inner",
-        },
+  -- vim.api.nvim_command('set foldmethod=expr')
+  -- vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
+  local filetypes = {
+    "markdown", "markdown_inline", "bash", "comment", "css", "dockerfile", "go", "graphql", "html",
+    "http", "java", "javascript",
+    "jsdoc", "json", "json5", "latex", "lua", "make", "perl", "python", "regex", "ruby", "rust", "scss", "tsx",
+    "typescript", "vim", "yaml", "typescriptreact"
+  }
+  require('nvim-treesitter').install(filetypes)
+
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = filetypes,
+    callback = function()
+      vim.treesitter.start()
+      vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.wo[0][0].foldmethod = 'expr'
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+  })
+end
+
+function config.nvim_treesitter_textobjects()
+  require("nvim-treesitter-textobjects").setup {
+    select = {
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+      -- You can choose the select mode (default is charwise 'v')
+      --
+      -- Can also be a function which gets passed a table with the keys
+      -- * query_string: eg '@function.inner'
+      -- * method: eg 'v' or 'o'
+      -- and should return the mode ('v', 'V', or '<c-v>') or a table
+      -- mapping query_strings to modes.
+      selection_modes = {
+        ['@parameter.outer'] = 'v', -- charwise
+        ['@function.outer'] = 'V',  -- linewise
+        ['@class.outer'] = '<c-v>', -- blockwise
       },
+      -- If you set this to `true` (default is `false`) then any textobject is
+      -- extended to include preceding or succeeding whitespace. Succeeding
+      -- whitespace has priority in order to act similarly to eg the built-in
+      -- `ap`.
+      --
+      -- Can also be a function which gets passed a table with the keys
+      -- * query_string: eg '@function.inner'
+      -- * selection_mode: eg 'v'
+      -- and should return true of false
+      include_surrounding_whitespace = false,
     },
   }
+  vim.keymap.set({ "x", "o" }, "af", function()
+    require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")
+  end)
+  vim.keymap.set({ "x", "o" }, "if", function()
+    require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
+  end)
+  vim.keymap.set({ "x", "o" }, "ac", function()
+    require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
+  end)
+  vim.keymap.set({ "x", "o" }, "ic", function()
+    require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
+  end)
+  vim.keymap.set({ "x", "o" }, "ab", function()
+    require "nvim-treesitter-textobjects.select".select_textobject("@block.outer", "textobjects")
+  end)
+  vim.keymap.set({ "x", "o" }, "ib", function()
+    require "nvim-treesitter-textobjects.select".select_textobject("@block.inner", "textobjects")
+  end)
 end
 
 return config
--- .config/nvim/lua/modules/lang/plugins.lua
+++ .config/nvim/lua/modules/lang/plugins.lua
@@ -2,17 +2,15 @@ local lang = {}
 local conf = require('modules.lang.config')
 
 lang['nvim-treesitter/nvim-treesitter'] = {
-  event = 'BufRead',
-  -- after = 'telescope.nvim',
   config = conf.nvim_treesitter,
-}
-
-lang['nvim-treesitter/nvim-treesitter-refactor'] = {
-  after = 'nvim-treesitter',
+  build = ':TSUpdate',
+  branch = 'main',
 }
 
 lang['nvim-treesitter/nvim-treesitter-textobjects'] = {
   after = 'nvim-treesitter',
+  config = conf.nvim_treesitter_textobjects,
+  branch = 'main',
 }
 
 return lang
